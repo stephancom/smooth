69a5cdfede7f33b349b56f34a474e9de1a69bfd9
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-11-20 01:10:06 -0600

    data directory setting

--- lib/smooth.rb
+++ lib/smooth.rb
@@ -8,6 +8,12 @@ module Smooth
   InvalidCollection ||= Class.new(Exception)
   InvalidRecord ||= Class.new(Exception)
 
+  mattr_accessor :data_directory
+
+  def self.data_directory
+    @@data_directory ||= Pathname.new(File.join(ENV['HOME'],'.smooth'))
+  end
+
   def self.namespace name="App", options={}
     name = name.camelize
     ns = Object.const_get(name) rescue nil
