ddd1bbf155f5f7f1babc48111e6b64ae3027f185
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-11-25 17:48:18 -0600

    updating redis backend

--- lib/smooth/backends/redis_backend.rb
+++ lib/smooth/backends/redis_backend.rb
@@ -1,40 +1,71 @@
-class Redis::HashKey
-  # Gets the value of a field
-  def [](field)
-    hget(field)
-  end
-
-  # Redis: HGET
-  def hget(field)
-    from_redis redis.hget(key, field), options[:marshal_keys][field]
-  end
-
-  def fetch field, *args, &block
-    value = hget(field)
-    default = args[0]
-
-    return value if value || (!default && !block_given?)
-
-    block_given? ? block.call(field) : default
-  end
-end
-
 class Smooth::RedisBackend < Smooth::MemoryBackend
   include Redis::Objects
 
   self.redis= Smooth.redis
 
   counter :id_counter, :start => 1
-  hash_key :record_storage, :marshal => true
 
   def records
-    record_storage
+    values = redis.hvals(redis_key)
+
+    values.map! do |value|
+      value = value.is_a?(String) ? (JSON.parse(value) rescue value) : value
+      value.symbolize_keys!
+      value
+    end
+
+    values
+  end
+
+  def all
+    records
+  end
+
+  def create attributes={}
+    obj = super(attributes)
+    obj.symbolize_keys!
+    redis.hset redis_key, obj[:id], JSON.generate(obj)
+    obj
+  end
+
+  def destroy id
+    id = id.is_a?(Hash) ? id.fetch(:id, id.fetch('id',nil)) : id
+    obj = id && show(id)
+    obj && redis.hdel(redis_key, id)
+    obj
+  end
+
+  def show id
+    value = redis.hget(redis_key, id)
+    value = value && JSON.parse(value)
+    value.symbolize_keys! if value.is_a?(Hash)
+
+    value
+  end
+
+  def update id, attributes={}
+    object = show(id)
+    object.symbolize_keys!
+    object.merge!(attributes.symbolize_keys)
+    redis.hset redis_key, id, JSON.generate(object)
+    object
+  end
+
+  def redis_key
+    id
   end
 
   protected
-    def process_records!
-      super
-      self.record_storage.fill(@records || {})
+    def delete!
+      redis.del(redis_key)
+    end
+
+    def redis_object_present?
+      redis.type(redis_key) == "hash"
+    end
+
+    def redis
+      self.class.redis
     end
 
     def assign_id_to object
